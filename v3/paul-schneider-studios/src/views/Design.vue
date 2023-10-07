9c94a05a4725f29708773475c0de9274397dba85
Paul Schneider <paul.schneider4@gmail.com>
2023-10-06 23:32:34 -0400

    Added design to vue 3.0 template

---
+++ v3/paul-schneider-studios/src/views/Design.vue
@@ -0,0 +1,375 @@
+<template>
+    
+    <div class="portfolio">
+      <!-- <Navigation /> -->
+      <div class="container ">
+        <div class="row">
+          <div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+            <img class="logo img-responsive" src="../assets/logo.png" >
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid brand intro">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <h1>My Portfolio</h1>
+              <p>Hello! I see you've stumbled across my design portfolio. I started this project in the winter of 2016. Im hoping to use this to jump start a career in graphic design, branding, UI design, or web design. I've been sketching illustrations since highschool. Back then, I was mostly re-imagining baseball logos (perhaps you see the see the influence in my portfolio logo?)  I periodically sketch to blowoff creative steam. After college, I began dreaming up business ideas. Get rich quick schemes seemed like such a romantic alternative to a life time of office work... This entreprenurial spirit culminated in myfoodstand.com, a site I designed from scratch. This portfolio is a collection of all these businesses ideas, and the illustrations, webpages, and videos I created to bring them to life. Its been fun!</p>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-5 col-md-offset-1">
+              <h1>My Methodology</h1>
+              <p>My approach to design is quite simple. Before I start any design or logo, I first ask myself, what ideas am I trying to communicate? I come up with a comprehensive list of themes I want my graphics to convey. They can be as tangable as "beer" or as subtle as "community". Second, I sketch designs that are complex enough to communicate as many of these themes as possible, but simple enough not to distract, confuse, or obfuscate. If not careful, these themes can drown eachother out. Finally, I use open source software to digitize my sketches. I use GIMP for raster images and Inkscape for vectors. </p>
+            </div>
+            <div class="col-md-4 col-md-offset-1">
+              <img class="logo-sketch img-responsive center-block" src="../assets/logo-sketch.png" >
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid brand voyaguer">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-5 col-md-offset-1">
+              <h1>Brand: Voyageur</h1>
+              <p>Voyageur is bar with a bookstore. Its both your local watering whole and your favorite library. Patrons can browse the aisles while enjoying a beer. Or recline in an arm chair with Mark Twain and a cocktail. Above all, Voyageur sells an adventurous spirit. The walls are obscured by old maps of the frontier and sweeping landscape paintings. The bookshelves are filled with travel, history, historical fiction, and American classics. And this public house would be just that. It's heavy oak coffee tables and squashy up-cycled furniture would encourage patrons to stop, relax, and put their feet up. Voyageur is a proud resident of Duluth Minnesota. Here, thrill seekers eager to explore Minnesota's boundary waters swap stories with old salts, returning from their most recent voyage across the great lakes. And everyone would whet their spirits for their next adventure north, like the legendary voyageurs before them.</p>
+            </div>
+            <div class="col-md-4 col-md-offset-1 margin-top-md-80">
+              <img class="img-responsive center-block" src="../assets/voyaguer-logo.png" style="max-height: 400px;" > 
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-5 col-md-offset-1">
+              <h2>Themes</h2>
+              <p>bar, bookstore, adventure, history, minnesota history, nature, exploration, folk tales</p>
+            </div>
+            <div class="col-md-4 col-md-offset-1 margin-top-md-30" >
+              <img class="img-responsive center-block" src="../assets/voyaguer-header.png" style="max-height: 400px;" > 
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-5 col-md-offset-1">
+              <h2>Design</h2>
+              <p>First, I wanted the logo to express a romantic vision of adventure and exploration. Second, I wanted this vision to connect with Minnesota's history as a remote outpost on the American Frontier. Thirdly, I wanted this image to tell a folk tale, as tall as Paul Bunyan. This is how I conceived my intrepid voyageur, sailing across a starry sky. The voyageur is gazing upward, straining to glimpse the northern lights. The scene is inscribed by a border and lettering, a convention common in older logos. The Copperplate font underscores this theme. I chose the tag line very carefully. Tall Tales & Ales expresses several of the themes: bar, bookstore, folksy, community. Finally the compass points along the outside border emphasizes the spirit of exploration.</p>
+            </div>
+            <div class="col-md-4 col-md-offset-1 margin-top-md-80 hidden-sm hidden-xs">
+              <img class="img-responsive center-block" src="../assets/simple-voyaguer-logo.png" style="max-height: 400px;" > 
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <img class="img-responsive center-block box-shadow" src="../assets/coasters.jpg">
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="container-fluid brand myfoodstand">
+          <div class="container">
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <h1>Brand: My Food Stand</h1>
+                <p>myfoodstand.com is a site that encourages sustainability and transparency within the Minneapolis food industry. It’s an online platform where consumers, producers, and businesses can come together and share exactly what they are eating, growing, and selling. Its not a marketplace; rather, its a resource that empowers the community to make better decisions. I designed everything for the site but the logo.</p>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <h2>Themes</h2>
+                <p>transparency, produce, farmers market, healthy, informative, simplicity, modern, bright</p>
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-md-10 col-md-offset-1" >      
+                <div id="MyFoodStandCarousel" class="carousel slide carousel-fade box-shadow" data-ride="carousel" data-interval=3500 data-pause=false>
+                  
+                <!-- Indicators -->
+                <!-- <ol class="carousel-indicators">
+                  <li data-target="#MyFoodStandCarousel" data-slide-to="0" class="active"></li>
+                  <li data-target="#MyFoodStandCarousel" data-slide-to="1"></li>
+                  <li data-target="#MyFoodStandCarousel" data-slide-to="2"></li>
+                </ol> -->
+              
+                  <!-- Wrapper for slides -->
+                  <div class="carousel-inner">
+                      <div class="item">
+                          <img src="../assets/myfoodstand-home.jpg" alt="Business Profile">
+                      </div>
+                    
+                      <div class="item active">
+                        <img src="../assets/myfoodstand-consumer-search.jpg" alt="Consumer Search">
+                      </div>
+                
+                      <div class="item">
+                        <img src="../assets/myfoodstand-consumer-index.jpg" alt="Consumer Tools">
+                      </div>
+                  </div>
+                
+                  <!-- Left and right controls -->
+                  <!-- <a class="left carousel-control" href="#MyFoodStandCarousel" data-slide="prev">
+                    <span class="glyphicon glyphicon-chevron-left"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="right carousel-control" href="#MyFoodStandCarousel" data-slide="next">
+                    <span class="glyphicon glyphicon-chevron-right"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div> -->
+
+              </div>
+              
+            </div>
+          </div>
+          
+          <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                <h2>Design</h2>
+                <p>For myfoodstand.com, I wanted to keep the focus on food. Fresh food. Good food. Local food. I wanted to transport the user to the aisle of their farmer’s market. I wanted to surround them with fruits and vegetables. To this end, I decided to feature large resolution pictures of produce in the left margin on all my pages. All the tools and content can be found on clean white forms on the right. These forms are minimalist - a design pattern very typical in modern web design. The form’s transparent border overlaps with the produce on the left, subtly underscoring the the site’s goal - to bring transparency to the food industry. </p>
+              </div>
+          </div>
+
+          <div class="row">
+            <div class="col-lg-8 col-lg-offset-2">
+              <div class="desktop" align="center">            
+                <div align="center" class="embed-responsive embed-responsive-16by9">
+                    <video controls class="embed-responsive-item">
+                        <source src="../assets/myfoodstand-elevator-speech.mp4" type="video/mp4">
+                    </video>
+                </div>          
+              </div>
+            </div>
+          </div>
+
+      </div>
+
+    </div>
+
+    <div class="container-fluid brand sewersurfer">
+        <div class="container">
+          
+          <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+              <img class="img-responsive center-block" src="../assets/sewer-surfer-logo.png">
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <h1>Brand: Sewer Surfer</h1>
+              <p>Sewer Surfer is a skate shop specializing in long board rentals. I borrowed this idea from <a href="https://www.instagram.com/weisestr.22/">Hase</a>, a quirky little shop I stumbled across in Berlin. It’s an excellent concept. They’re located just a couple blocks from Templehof, a gigantic airport-turned-park. For only a few euro, I was cruising down the runway on one of their decks. Hase also rents picnic blankets, guitars, sun glasses, frisbees - anything else you might need for a sunny day at the park. Sewer Surfer is their American cousin. It’s a brand that captures the thrill of carving a huge hill.</p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <h2>Themes</h2>
+              <p>skating, carving, surfing, underground, bold</p>
+            </div>
+          </div>
+
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <h2>Design</h2>
+              <p>I wanted my design to recall the iconic logos that rule the skating world. It must stand toe-to-toe with such classics like Independent, Habitat, Spitfire, and OJ while bringing its own style to the fight. I believe my Sewer Surfer brand does just that. The big double S carving it’s way down the center screams skateboarding. The name “Sewer Surfer” paints a portrait of the grungy, underground surfers I am targeting. Its also a direct homage to Turtles in Time, an arcade game I binged when I was little. Its an obscure reference, but one I hold dear. I hope those who grasp it appreciate the goofy exhilaration of TMNT and associate those qualities with my brand. I submerged all of this within a street sign. Its a warning to everyone on the road - Yeild! You’re at a surfer crossing! The lettering is relaxed. The contrast is bold. This logo is at home beneath your deck, on your hat, or tattooed across your arm.</p>
+            </div>
+          </div>
+          
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <img class="img-responsive center-block box-shadow" src="../assets/sewer-wall.jpg">
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+</template>
+
+<script setup> 
+//import Navigation from "@/components/Navigation.vue";
+</script>
+
+<style scoped>
+
+.logo {
+    margin-top: 20%;
+    margin-bottom: 150px;
+}
+
+.matte {
+
+    background-image: url(/public/img/beige-cardstock.jpg); 
+    padding: 5px;
+    border-radius: 2px;  
+    /* box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.9), 0 6px 20px 0 rgba(0, 0, 0, 0.85);*/
+}
+
+.photo {
+    max-height: 100%;
+}
+
+.portfolio {
+    font-family: sans-serif;
+    color: #312f2f;
+    background: url("../assets/navy-cloth.jpg");
+}
+
+.portfolio .row {
+    margin-top: 30px;
+}
+
+.portfolio h1, .portfolio h2 {
+    font-size: 30px;
+}
+
+.portfolio p {
+    margin: 25px 0px;
+    line-height: 27px;
+}
+
+/* Desktop */
+@media (min-width : 768px) {
+    .portfolio p {
+        margin: 30px 0px;
+    }
+}
+
+.brand {
+    padding: 50px 0;
+}
+
+.intro {
+    background-color: #ffffff;
+}
+
+.logo-sketch {
+    padding: 25px;
+}
+
+/* Desktop */
+@media (min-width : 768px) {
+    .logo-sketch {
+        padding: 0px;
+    }
+}
+
+.voyaguer {
+    background-color: #dddddd;
+}
+
+.myfoodstand {
+    background-color: #ffffff;
+}
+
+.desktop {
+    padding: 3%;
+    width: 100%;
+    height: 800px;
+    max-height: 800px;
+    background-image: url('/public/img/desktop.png');
+    background-repeat: no-repeat;
+    background-size: contain;
+}
+
+.carousel-fade .carousel-inner .item {
+    transition-property: opacity;
+}
+
+.carousel-fade .carousel-inner .item {
+    opacity: 0;
+    transition-property: opacity;
+}
+  
+.carousel-fade .carousel-inner .active {
+    opacity: 1;
+}
+
+.carousel-fade .carousel-inner .active.left,
+.carousel-fade .carousel-inner .active.right {
+    left: 0;
+    opacity: 0;
+    z-index: 1;
+}
+
+.carousel-fade .carousel-inner .next.left,
+.carousel-fade .carousel-inner .prev.right {
+    opacity: 1;
+}
+
+.carousel-fade .carousel-control {
+    z-index: 2;
+}
+
+/*
+WHAT IS NEW IN 3.3: "Added transforms to improve carousel performance in modern browsers."
+now override the 3.3 new styles for modern browsers & apply opacity
+*/
+@media all and (transform-3d), (-webkit-transform-3d) {
+    .carousel-fade .carousel-inner > .item.next,
+    .carousel-fade .carousel-inner > .item.active.right {
+    opacity: 0;
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    }
+    .carousel-fade .carousel-inner > .item.prev,
+    .carousel-fade .carousel-inner > .item.active.left {
+    opacity: 0;
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    }
+    .carousel-fade .carousel-inner > .item.next.left,
+    .carousel-fade .carousel-inner > .item.prev.right,
+    .carousel-fade .carousel-inner > .item.active {
+    opacity: 1;
+    -webkit-transform: translate3d(0, 0, 0);
+            transform: translate3d(0, 0, 0);
+    }
+}
+
+.sewersurfer {
+    background-color: #ffff00;
+}
+
+/* General Styles */
+
+.margin-top-10 {
+    margin-top:10px;
+}
+
+.margin-top-20 {
+    margin-top:20px;
+}
+
+.margin-top-30 {
+    margin-top:30px;
+}
+
+.margin-top-md-10 {
+    margin-top:10px;
+}
+
+.margin-top-md-20 {
+    margin-top:20px;
+}
+
+.margin-top-md-30 {
+    margin-top:30px;
+}
+
+.margin-top-md-80 {
+    margin-top:80px;
+}
+
+.box-shadow {
+    -webkit-box-shadow: 1px 4px 16px 2px rgba(50, 50, 50, 0.75);
+    -moz-box-shadow:    1px 4px 16px 2px rgba(50, 50, 50, 0.75);
+    box-shadow:         1px 4px 16px 2px rgba(50, 50, 50, 0.75);
+}
+
+</style>
+
+
